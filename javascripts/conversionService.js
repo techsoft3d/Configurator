746d71daa49d2b9ba77c70a78a26dd91d1eb82ee
Ricky Johnson <ricky.johnson@techsoft3d.com>
2022-11-03 14:23:22 -0400

    initial commit

---
+++ javascripts/conversionService.js
@@ -0,0 +1,31 @@
+modelUIDs = [
+        "707947a9-7df8-4720-bafd-e2d9b3a0d020", //vimek
+        "38d50b36-2633-406c-b51a-026ac86874c5", //steering 1
+        "57ef57b9-5249-47c9-875a-e92a8664b111", //engine 1
+]
+
+async function startViewer(modelName) {
+        const conversionServiceURI = "https://csapi.techsoft3d.com";
+
+        var viewer;
+
+        let res = await fetch(conversionServiceURI + '/api/streamingSession');
+        var data = await res.json();
+
+        await fetch(conversionServiceURI + '/api/enableStreamAccess/' + data.sessionid, { method: 'put', headers: { 'items': JSON.stringify(modelUIDs) } });
+
+        viewer = new Communicator.WebViewer({
+                containerId: "viewerContainer",
+                endpointUri: 'wss://' + data.serverurl + ":" + data.port + '?token=' + data.sessionid,
+                model: modelName,
+                streamingMode: Communicator.StreamingMode.OnDemand,
+                boundingPreviewMode: 'none',
+                enginePath: "https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@2022.2",
+                rendererType: 0
+        });
+
+        viewer.start();
+
+        return viewer;
+
+}
